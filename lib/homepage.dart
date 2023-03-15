5349c672db77dd02dcd5582f9c655bf2e4c684ca
Pankaj Barad <pankajbarad18@gmail.com>
2023-03-15 23:03:08 +0530

    Day1 Done

---
+++ lib/homepage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    int i = 50;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog Bar"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome $i"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
