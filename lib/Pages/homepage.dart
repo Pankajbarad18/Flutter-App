5766f72410d2321c7b1c78c16309423d100586d7
Pankaj Barad <pankajbarad18@gmail.com>
2023-04-06 21:50:20 +0530

    Homepage Completed

--- lib/Pages/homepage.dart
+++ lib/Pages/homepage.dart
@@ -1,23 +1,153 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:velocity_x/velocity_x.dart';
 
-import '../utils/Widgets/drawer.dart';
+import 'package:app/Models/App.dart';
+import 'package:app/utils/Widgets/themes.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    var json = await rootBundle.loadString("assets/Files/Items.json");
+    var decode = jsonDecode(json);
+    var productsdata = decode["products"];
+    Model.products = List.from(productsdata)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    int i = 50;
+    // final fakeitem = List.generate(25, (index) => Model.products[0]);
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Catalog App"),
-      ),
-      body: Center(
+      backgroundColor: MyTheme.creamColor,
+      body: SafeArea(
         child: Container(
-          child: Text("Welcome $i"),
+          padding: Vx.m32,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Header(),
+              if (Model.products != null && Model.products.isNotEmpty)
+                CatalogList().expand()
+              else
+                Center(
+                  child: CircularProgressIndicator(),
+                )
+            ],
+          ),
         ),
       ),
-      drawer: MyDrawer(),
+    );
+  }
+}
+
+class CatalogList extends StatelessWidget {
+  const CatalogList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: Model.products.length,
+      itemBuilder: (context, index) {
+        final catalog = Model.products[index];
+        return CatalogItem(catalog: catalog);
+      },
+    );
+  }
+}
+
+class CatalogItem extends StatelessWidget {
+  final Item catalog;
+
+  const CatalogItem({
+    Key? key,
+    required this.catalog,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return VxBox(
+        child: Row(
+      children: [
+        CatImage(image: catalog.image),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              catalog.name.text.lg.color(MyTheme.darkbluishColor).make(),
+              catalog.desc.text.textStyle(context.captionStyle).bold.make(),
+              6.heightBox,
+              ButtonBar(
+                alignment: MainAxisAlignment.spaceBetween,
+                buttonPadding: Vx.mH8,
+                children: [
+                  "\$${catalog.price}".text.xl.bold.make(),
+                  ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                            MyTheme.darkbluishColor,
+                          ),
+                          shape: MaterialStateProperty.all(StadiumBorder())),
+                      child: "Buy".text.make())
+                ],
+              ).pOnly(right: 8)
+            ],
+          ),
+        )
+      ],
+    )).white.roundedLg.square(130).make().py16();
+  }
+}
+
+class CatImage extends StatelessWidget {
+  const CatImage({super.key, required this.image});
+  final String image;
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(image)
+        .box
+        .rounded
+        .p8
+        .color(MyTheme.creamColor)
+        .make()
+        .p16()
+        .w40(context);
+  }
+}
+
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        "Catalog App".text.xl4.bold.color(MyTheme.darkbluishColor).make(),
+        "Trending Products".text.xl.bold.make(),
+      ],
     );
   }
 }
